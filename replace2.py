8b17b039db7889c55acb2df6ff4fad762d688fd8
ssydorov <ssydorov@gmail.com>
2016-12-10 22:42:33 +0200

    comments added

--- replace2.py
+++ replace2.py
@@ -8,18 +8,28 @@ year = "2016"
 month = "09"
 # FileNameInput имя входного файла
 FileNameInput = "invoice_" + company_code + "_" + year + "_" + month + ".txt"
-# FileNameInput = "input.txt"
+FileNameInput = "input.txt"
 # FileNameOutput имя результирующего файла
 FileNameOutput = "output.txt"
+# словарь словосочетаний нуждающихся в замене для коррекной работы парсера
 replacements = {'"Hello World"' : 'Hello World', 'world' : 'World',  'hello' : 'Hello'}
-
+# чтение исходного файла
 f =open(FileNameInput).read()
 newText = f
+# замена словосочетаний
 for i in replacements.keys():
     newText = newText.replace(i, replacements[i])
-    print(i, replacements[i])
 
 
+# запись файла
 with open(FileNameOutput, "w") as f:
     f.write(newText)
 f.close()
+
+# открытие файла для парсинга
+
+# парсинг файла
+
+# запись общей статистики
+
+# запись детализации
